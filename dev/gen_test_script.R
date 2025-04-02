ef20bde5d80d57133f504a1b3e40efaf71b88337
theial-remains <preethinagasuru@gmail.com>
2025-04-02 08:38:24 -0400

    add some stuff

--- dev/gen_test_script.R
+++ dev/gen_test_script.R
@@ -50,7 +50,7 @@ sd_store_articles(
   article_data = scraped_data2,
   news_site = "huffpost",
   overwrite = FALSE
-) # TODO should add tictoc to this
+) # FIXME should add tictoc to this
 
 
 
@@ -60,18 +60,18 @@ library(dplyr)
 library(lubridate)
 index_path <- "inst/extdata/article_data/huffpost/index.json"
 index_df <- read_json(index_path, simplifyVector = TRUE)
-View(index_df)
+head(index_df)
 
 # identify invalid or misformatted dates
 bad_dates <- index_df %>%
   mutate(valid_format = grepl("^\\d{4}-\\d{2}-\\d{2}$", published_date)) %>%
   filter(!valid_format)
-View(bad_dates)
+head(bad_dates)
 
 # fix bad dates
 good_dates <- bad_dates %>%
   sd_clean_date()
-View(good_dates)
+head(good_dates)
 
 # store good dates
 sd_store_articles(
@@ -81,9 +81,9 @@ sd_store_articles(
 ) # Why so slow :(
 
 
-# TODO
-# TODO
-# TODO fuckin asap
+# FIXME
+# FIXME
+# FIXME fuckin asap
 # pretty sure new/updated rows reporting aint working correctly
 # for sd_store_articles
 # and it slow af gotta fix that
@@ -91,13 +91,15 @@ sd_store_articles(
 
 # hopefully works now
 sampled_urls <- sd_sample_urls(
-  start_date = "2021-01-01", # random aah date range,
-  end_date = "2021-12-01",
+  start_date = "2017-01-01", # random aah date range,
+  end_date = "2025-03-01",
   news_site = "huffpost",
   number = 100,
   period = "month"
 )
 View(sampled_urls)
+
+# TODO get random sample and do monthly sentiment analysis
 
 # check <- sd_pull_articles(start_date = "2024-01-01",
 #                           end_date = "2024-01-31",
@@ -105,13 +107,13 @@ View(sampled_urls)
 #                           url = TRUE)
 # View(check)
 
-# sampled_urls2 <- gu_remove_duplicates(article_urls)
-
 scraped_data <- sa_scrape_articles(sampled_urls)
-View(scraped_data)
+str(scraped_data)
 
 sentiment_data <- as_article_sentiment(scraped_data)
 View(sentiment_data)
+
+saveRDS(sentiment_data, file = "random_shit/all_years_sentiment_sample.RDS")
 
 # now u can store random sample of sentiment data
 # or analyze it or make plots, whatever u want
@@ -120,3 +122,6 @@ sd_store_articles(
   news_site = "huffpost",
   overwrite = FALSE
 )
+
+# analysis of sample
+sentiment_data_sample <- readRDS("random_shit/all_years_sentiment_sample.RDS")
